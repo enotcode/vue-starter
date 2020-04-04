1b177e14e191a60078e7b5c59d12daf648813f7e
Viktor <enotcode@gmail.com>
2020-04-04 21:41:56 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+<v-app>
+    <v-app-bar app clipped-left>
+      <v-toolbar-title>App</v-toolbar-title>
+    </v-app-bar>
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+</v-app>
+</template>
